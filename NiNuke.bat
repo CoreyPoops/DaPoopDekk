4da675f1420370b024ec4f869390047eb13e16fc
Corye <99693554+CoreyPoops@users.noreply.github.com>
2022-02-14 17:21:57 -0500

    Prize Pozzezzion
    Glory

---
+++ NiNuke.bat
@@ -0,0 +1,8 @@
+@echo off
+pause
+color 0a
+mode 1000
+
+:a
+echo nigger %random% nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger nigger
+goto a
